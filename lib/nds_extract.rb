012bdda470fd3d3b0398bd81b7023aa1b861ccaa
Christine Bumpous <bumpouce@gmail.com>
2020-01-24 22:18:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -73,18 +73,7 @@ def gross_per_studio(collection)
   studio_index = 0 
   studio_gross_hash = {}
 
-  d = 0 
-  miramax_count = 0
-  while d < directors_database.length do
-    m = 0
-    while m < directors_database[d][:movies].count do
-      if directors_database[d][:movies][m][:studio] == "Miramax"
-        pp "Given: #{directors_database[d][:movies][m][:studio]} #{directors_database[d][:movies][m][:title]} #{directors_database[d][:movies][m][:worldwide_gross]} "
-      end
-      m += 1
-    end
-    d += 1
-  end
+  pp "#{collection}"
  
   while studio_index < collection.length do
     gross = 0 
@@ -98,10 +87,6 @@ def gross_per_studio(collection)
       studio_name = find_gross_by_director_and_title(director, title)[0]
       gross = find_gross_by_director_and_title(director, title)[1]
 
-      if studio_name == "Miramax"
-        pp "Found: #{studio_name} #{title} #{gross}"
-      end
-      
     #else given studio + gross
     else
       studio_name = collection[studio_index][:studio]
