db9a1722d841885125a68d6ddd85965100975ccf
Christine Bumpous <bumpouce@gmail.com>
2020-01-25 00:06:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -55,13 +55,11 @@ def find_gross_by_director_and_title (director, title)
       found = false
       
       while b < directors_database[a][:movies].length && !found do
-        if !directors_database[a][:movies][b][:counted]
-          if directors_database[a][:movies][b][:title] == title 
-            retrieved << directors_database[a][:movies][b][:studio]
-            retrieved << directors_database[a][:movies][b][:worldwide_gross]
-            directors_database[a][:movies][b][:counted] = true
-            found = true
-          end
+        if directors_database[a][:movies][b][:title] == title 
+          retrieved << directors_database[a][:movies][b][:studio]
+          retrieved << directors_database[a][:movies][b][:worldwide_gross]
+          directors_database[a][:movies][b][:counted] = true
+          found = true
         end
         b += 1
       end
